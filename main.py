49d58d8b20a2a15e9cd98d01a2e3502445209b1a
Andrei-Raev <51417538+Andrei-Raev@users.noreply.github.com>
2020-09-23 16:31:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,57 @@
-print('Hello World!')
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'aaa.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(749, 609)
+        font = QtGui.QFont()
+        font.setFamily("Comic Sans MS")
+        font.setPointSize(12)
+        MainWindow.setFont(font)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.main_text = QtWidgets.QLabel(self.centralwidget)
+        self.main_text.setMinimumSize(QtCore.QSize(0, 500))
+        self.main_text.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.main_text.setObjectName("main_text")
+        self.verticalLayout.addWidget(self.main_text)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.horizontalLayout.addWidget(self.pushButton_3)
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.horizontalLayout.addWidget(self.pushButton_4)
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Игра"))
+        self.main_text.setText(_translate("MainWindow", "Здесь появится ваш текст"))
+        self.pushButton_3.setText(_translate("MainWindow", "Идти"))
+        self.pushButton_4.setText(_translate("MainWindow", "???"))
+        self.pushButton_2.setText(_translate("MainWindow", "Ок"))
+        self.pushButton.setText(_translate("MainWindow", "Отмена"))
