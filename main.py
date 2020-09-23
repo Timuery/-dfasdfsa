a22660574989cbd1536bd9d50aa8d926d832902d
Andrei-Raev <51417538+Andrei-Raev@users.noreply.github.com>
2020-09-23 17:11:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,46 +7,46 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(749, 609)
-        self.setFixedSize(749, 609)
-        
+        MainWindow.setFixedSize(749, 609)
+
         font = QtGui.QFont()
         font.setFamily("Comic Sans MS")
         font.setPointSize(12)
-        
+
         MainWindow.setFont(font)
-        
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
         self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName("verticalLayout")
-        
+
         self.main_text = QtWidgets.QLabel(self.centralwidget)
         self.main_text.setMinimumSize(QtCore.QSize(0, 500))
-        self.main_text.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.main_text.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
         self.main_text.setObjectName("main_text")
-        
+
         self.verticalLayout.addWidget(self.main_text)
-        
+
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        
+
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_3.setObjectName("pushButton_3")
         self.horizontalLayout.addWidget(self.pushButton_3)
-        
+
         self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_4.setObjectName("pushButton_4")
         self.horizontalLayout.addWidget(self.pushButton_4)
-        
+
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setObjectName("pushButton_2")
         self.horizontalLayout.addWidget(self.pushButton_2)
-        
+
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setObjectName("pushButton")
         self.horizontalLayout.addWidget(self.pushButton)
-        
+
         self.verticalLayout.addLayout(self.horizontalLayout)
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -56,24 +56,40 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Игра"))
-        
-        self.main_text.setText(_translate("MainWindow", "Говно"))
-        
+
+        self.main_text.setText(_translate("MainWindow", "тест"))
+
         self.pushButton_3.setText(_translate("MainWindow", "Идти"))
         self.pushButton_4.setText(_translate("MainWindow", "Поесть"))
         self.pushButton_2.setText(_translate("MainWindow", "Сделать"))
         self.pushButton.setText(_translate("MainWindow", "Выйти"))
 
-        self.pushButton.clicked.connect(self.first_button)
-        
-    def first_button(self, MainWindwow):
-        text = 'лох'
+        self.pushButton.clicked.connect(self.exit_prog)
+        self.pushButton_4.clicked.connect(self.eat)
+        self.pushButton_3.clicked.connect(self.go)
+        self.pushButton_2.clicked.connect(self.do)
+
+    def exit_prog(self):
+        self.close()
+
+    def eat(self):
+        text = 'Хрум '
         self.main_text.setText(self.main_text.text() + f'{text}')
+        if len(self.main_text.text().split('\n')[-1]) >= 50:
+            self.main_text.setText(self.main_text.text() + '\n')
+
+    def do(self):
+        self.main_text.setText('')
+
+    def go(self):
+        self.setFixedSize(500, 500)
+        self.resize(500, 500)
+
+
 class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
 
 
 if __name__ == '__main__':
